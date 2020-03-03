5018a8d3321c1409d23c31966c1e78bd32d3579a
Hyan18 <HarrisonYan18@gmail.com>
2020-03-03 20:49:48 +0000

    refactor: make tests compact

--- FizzBuzz/FizzBuzzTest.cs
+++ FizzBuzz/FizzBuzzTest.cs
@@ -10,80 +10,37 @@ namespace FizzBuzz
         }
 
         [Test]
-        public void Return_1_When_Given_1()
+        public void Return_Itself_When_Given_Number_Not_Divisible_By_3_Or_5(
+            [Values(1, 2, 4, 7, 8)] int input)
         {
-            int input = 1;
-
             string output = FizzBuzz.Value(input);
 
-            Assert.AreEqual("1", output);
+            Assert.AreEqual(input.ToString(), output);
         }
 
-        [Test]
-        public void Return_2_When_Given_2()
-        {
-            int input = 2;
-
-            string output = FizzBuzz.Value(input);
-
-            Assert.AreEqual("2", output);
-        }
 
         [Test]
-        public void Return_Fizz_When_Given_3()
+        public void Return_Fizz_When_Given_Multiple_Of_3(
+            [Values(3, 6, 9)] int input)
         {
-            int input = 3;
-
             string output = FizzBuzz.Value(input);
 
             Assert.AreEqual("Fizz", output);
         }
 
         [Test]
-        public void Return_Fizz_When_Given_6()
+        public void Return_Buzz_When_Given_Multiple_Of_5(
+            [Values(5, 10, 20)] int input)
         {
-            int input = 6;
-
-            string output = FizzBuzz.Value(input);
-
-            Assert.AreEqual("Fizz", output);
-        }
-
-        [Test]
-        public void Return_Buzz_When_Given_5()
-        {
-            int input = 5;
-
             string output = FizzBuzz.Value(input);
 
             Assert.AreEqual("Buzz", output);
         }
 
         [Test]
-        public void Return_Buzz_When_Given_10()
+        public void Return_FizzBuzz_When_Given_Multiple_Of_15(
+            [Values(15, 30, 45)] int input)
         {
-            int input = 10;
-
-            string output = FizzBuzz.Value(input);
-
-            Assert.AreEqual("Buzz", output);
-        }
-
-        [Test]
-        public void Return_FizzBuzz_When_Given_15()
-        {
-            int input = 15;
-
-            string output = FizzBuzz.Value(input);
-
-            Assert.AreEqual("FizzBuzz", output);
-        }
-
-        [Test]
-        public void Return_FizzBuzz_When_Given_30()
-        {
-            int input = 30;
-
             string output = FizzBuzz.Value(input);
 
             Assert.AreEqual("FizzBuzz", output);
